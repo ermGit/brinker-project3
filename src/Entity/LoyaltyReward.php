ab8fa0612c1a68ab3381d8633492d4ddfc3fd3d3
Eric Moore <emoore@thunkit.biz>
2025-08-04 23:12:35 -0500

    main: Add phpDoc comments to LoyaltyReward

--- src/Entity/LoyaltyReward.php
+++ src/Entity/LoyaltyReward.php
@@ -16,16 +16,26 @@ class LoyaltyReward
     #[ORM\Column]
     private array $reward = [];
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return array
+     */
     public function getReward(): array
     {
         return $this->reward;
     }
 
+    /**
+     * @param array $reward
+     * @return $this
+     */
     public function setReward(array $reward): static
     {
         $this->reward = $reward;
@@ -33,6 +43,9 @@ class LoyaltyReward
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getRewardName(): string
     {
         return $this->reward['name'] ?? 'Unnamed Reward';
